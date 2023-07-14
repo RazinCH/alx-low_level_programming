9d899285657fc938eb719c258ab243ca31f336a9
RazinCH <cheb.nizar@gmail.com>
2023-07-14 12:24:43 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+
+	{
+		if (n != 'e' && n != 'q')
+
+		{
+
+			putchar(n);
+
+		}
+
+	};
+
+	putchar('\n');
+	return (0);
+}
+
