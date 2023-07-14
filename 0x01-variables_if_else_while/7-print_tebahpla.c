59116892149d2a4d6c0d73e17affa65e15005ae7
RazinCH <cheb.nizar@gmail.com>
2023-07-14 12:58:56 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+
+	{
+		putchar(n);
+	};
+
+	putchar('\n');
+	return (0);
+}
+
