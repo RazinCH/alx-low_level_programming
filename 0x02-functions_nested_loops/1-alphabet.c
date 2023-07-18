cba3fb7b89a23943ef999662bbe36399ab9b2736
RazinCH <cheb.nizar@gmail.com>
2023-07-18 11:58:15 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Enter code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char y = 'a';
+
+	while (y <= 'z')
+	{
+		_putchar(y);
+		y++;
+	}
+
+		_putchar('\n');
+
+}
+
