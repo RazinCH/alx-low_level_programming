d5807fbca92c650ab9aa3b9eee13c7f2272c93e9
RazinCH <cheb.nizar@gmail.com>
2023-07-24 12:16:07 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s : string to evaluate
+ *
+ * Return : length of a string
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	return (n);
+}
