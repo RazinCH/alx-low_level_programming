b0c5fa395f34ab4ffed5a8ebe6a511bc64db43e8
RazinCH <cheb.nizar@gmail.com>
2023-08-17 09:01:25 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This projecy will be about Variadic functions in the language C.
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef var_func_h
+#define var_func_h
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
